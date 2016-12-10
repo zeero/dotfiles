52bf0aae75d131e5eaf2d0a560d6ee83537c3011
zeero <zeero26@gmail.com>
2016-12-10 19:20:29 +0900

    rubyインストール手順追加

--- install.sh
+++ install.sh
@@ -22,6 +22,12 @@ git config --global alias.graph "log --graph --decorate --name-status"
 # HomeBrew
 brew bundle
 
+# ruby
+brew link readline --force
+CONFIGURE_OPTS="--with-readline-dir=/usr/local/opt" rbenv install 2.3.1
+rbenv global 2.3.1
+gem install bundler cocoapods ruboty
+
 # node.js
 npm install -g coffee-script jshint hubot hubot-generator yo
 
