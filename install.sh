57051d2ba31a1140a48036efa48e536855fedbc9
zeero <zeero26@gmail.com>
2016-12-10 15:50:51 +0900

    npmとheroku追加

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# git-submodules
+git submodule update --init
+
+# symlink
+
+# git config
+git config --global user.name zeero
+git config --global user.email zeero26@gmail.com
+git config --global github.user zeero
+git config --global push.default matching
+git config --global http.sslVerify false
+git config --global core.excludesfile ~/.gitignore
+git config --global core.editor vim
+git config --global diff.tool vimdiff
+git config --global diff.algorithm histogram
+git config --global alias.graph "log --graph --decorate --name-status"
+
+# Ricty
+
+# HomeBrew
+brew bundle
+
+# node.js
+npm install -g coffee-script jshint hubot hubot-generator yo
+
