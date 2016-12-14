0bec08d6f48eabcf8dad345ae13ccc6ff3707817
zeero <zeero26@gmail.com>
2016-12-14 13:42:20 +0900

    install.bat見直し中

--- install.bat
+++ install.bat
@@ -1,6 +1,19 @@
 @echo off
 
+rem chocolatey
+rem まずは以下のコマンドを実行して、chocolateyをインストールする
+rem @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+
+rem choco_install.batを実行
+
+rem インストールが終わったら、dotfilesを落とします
+rem git clone https://github.com/zeero/dotfiles.git
+
+
 cd /d %~dp0
+
+rem git submodule
+git submodule update --init
 
 rem symlink
 move %HOMEDRIVE%%HOMEPATH%\vimperator %HOMEDRIVE%%HOMEPATH%\vimperator.org
@@ -10,22 +23,31 @@ mklink %HOMEDRIVE%%HOMEPATH%\_vimrc %~dp0\vim\vimrc
 mklink %HOMEDRIVE%%HOMEPATH%\_vimperatorrc %~dp0\vimperator\vimperatorrc
 mklink %HOMEDRIVE%%HOMEPATH%\.gitignore %~dp0\home\.gitignore
 mklink %HOMEDRIVE%%HOMEPATH%\.ctags %~dp0\home\.ctags
+rem msys2
+mklink %HOMEDRIVE%%HOMEPATH%\lib\msys32\home\%USERNAME%\.minttyrc %~dp0\msys2\home\.minttyrc
+move %HOMEDRIVE%%HOMEPATH%\lib\msys32\etc\pacman.conf %HOMEDRIVE%%HOMEPATH%\lib\msys32\etc\pacman.conf.org
+mklink %HOMEDRIVE%%HOMEPATH%\lib\msys32\etc\pacman.conf %~dp0\msys2\etc\pacman.conf
 pause
-
-rem git submodules (NeoBundle, etc)
-git submodule update --init
+echo.
 
 rem git configuration
 git config --global user.name zeero
 git config --global user.email zeero26@gmail.com
-git config --global github.user zeero
-git config --global push.default matching
+git config --global push.default simple
 git config --global http.sslVerify false
 git config --global core.excludesfile ~/.gitignore
 git config --global core.editor vim
 git config --global diff.tool vimdiff
 git config --global diff.algorithm histogram
+git config --global merge.tool vimdiff
+git config --global alias.co "checkout"
+git config --global alias.st "status"
 git config --global alias.graph "log --graph --decorate --name-status"
+git config --global alias.ignore "update-index --skip-worktree"
+git config --global alias.noignore "update-index --no-skip-worktree"
+git config --global alias.stashdiff "diff HEAD..stash@{0}"
+pause
+echo.
 
 rem Ricty
 echo RictyDiminishedをインストールしてください。
@@ -36,22 +58,21 @@ explorer lib\RictyDiminished\RictyDiminished-BoldOblique.ttf
 pause
 echo.
 
-rem chocolatey
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
-echo コマンドプロンプトを再起動して、choco_install.batを実行してください。
+rem vimproc
+echo NeoBundleInstallでvimprocのコンパイルが通るはず。
+echo （うまくいかなかったら「make -f make_mingw32.mak」を実行）
+pause
+echo.
+
 echo 別途インストールが必要なソフトウェア
+echo ・mactype（chocolateyパッケージが壊れている）
+echo 　　https://github.com/snowie2000/mactype/releases
 echo ・cmigemo
 echo 　　http://www.kaoriya.net/software/cmigemo/
 echo ・nkf
 echo 　　http://www.vector.co.jp/soft/win95/util/se295331.html
 echo ・mktemp
 echo 　　http://gnuwin32.sourceforge.net/packages/mktemp.htm
-pause
-echo.
-
-rem vimproc
-echo NeoBundleInstallでvimprocのコンパイルが通るはず。
-echo （うまくいかなかったら「make -f make_mingw32.mak」を実行）
 pause
 echo.
 
