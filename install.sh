d71727a3d82005ce7b1ad953cbd0cb03797e9020
zeero <zeero26@gmail.com>
2016-12-24 11:36:34 +0900

    add bin symlink

--- install.sh
+++ install.sh
@@ -18,6 +18,22 @@ ln -s `dirname $0`/home/.git-prompt-colors.sh ~/.git-prompt-colors.sh
 ln -s `dirname $0`/home/.tmux.conf ~/.tmux.conf
 ln -s `dirname $0`/home/.irbrc ~/.irbrc
 ln -s `dirname $0`/home/.ctags ~/.ctags
+chmod 744 `dirname $0`/home/bin/alcatraz_reload
+chmod 744 `dirname $0`/home/bin/fcd
+chmod 744 `dirname $0`/home/bin/mkdatedir
+chmod 744 `dirname $0`/home/bin/ug
+chmod 744 `dirname $0`/lib/git-info/git-info
+chmod 744 `dirname $0`/lib/git-info/git-editor
+chmod 744 `dirname $0`/lib/git-info/git-pager
+chmod 744 `dirname $0`/lib/markdown2ctags/markdown2ctags.py
+ln -s `dirname $0`/home/bin/alcatraz_reload ~/bin/alcatraz_reload
+ln -s `dirname $0`/home/bin/fcd ~/bin/fcd
+ln -s `dirname $0`/home/bin/mkdatedir ~/bin/mkdatedir
+ln -s `dirname $0`/home/bin/ug ~/bin/ug
+ln -s `dirname $0`/lib/git-info/git-info ~/bin/git-info
+ln -s `dirname $0`/lib/git-info/git-editor ~/bin/git-editor
+ln -s `dirname $0`/lib/git-info/git-pager ~/bin/git-pager
+ln -s `dirname $0`/lib/markdown2ctags/markdown2ctags.py ~/bin/markdown2ctags.py
 
 # git config
 git config --global user.name zeero
