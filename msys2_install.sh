bd33e519f10a4a6f1ec3d2667eb79de6c4af8e72
zeero <zeero26@gmail.com>
2017-01-20 20:32:13 +0900

    git-for-windows-sdkに移行

--- msys2_install.sh
+++ msys2_install.sh
@@ -1,21 +1,24 @@
+# コメントアウトはgit-for-windows-sdkでは不要ぽいもの
+
 # setup
-pacman-key --init
-pacman -Syuu
+# pacman-key --init
+# pacman -Syuu
 
 # install packages
-pacman -S mingw-w64-i686-toolchain
-pacman -S base-devel
+# pacman -S mingw-w64-i686-toolchain
+# pacman -S base-devel
 pacman -S msys2-devel
 pacman -S tmux-git
-pacman -S openssh
-pacman -S vim
-pacman -S mingw-w64-i686-curl
-pacman -S mingw-w64-i686-ruby
-pacman -S mingw-w64-i686-lua
-pacman -S mingw-w64-i686-python2
-pacman -S mingw-w64-i686-python3
-pacman -S mingw-w64-i686-perl
+# pacman -S openssh
+# pacman -S vim
+# pacman -S mingw-w64-i686-curl
 pacman -S mingw-w64-i686-ag
 pacman -S mingw-w64-i686-jq
 pacman -S mingw-w64-i686-universal-ctags-git
 
+pacman -S mingw-w64-i686-ruby
+pacman -S mingw-w64-i686-lua
+# pacman -S mingw-w64-i686-python2
+# pacman -S mingw-w64-i686-python3
+# pacman -S mingw-w64-i686-perl
+#
