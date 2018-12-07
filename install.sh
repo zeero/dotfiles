556904e12fe21ae5d3a93aa0f6e06cc57a78bc4a
zeero <zeero26@gmail.com>
2018-12-07 18:47:59 +0900

    update_xcode_plugins実行をスクリプトに組み込み

--- install.sh
+++ install.sh
@@ -105,8 +105,9 @@ gem specific_install -l http://github.com/zeero/ruboty-gen.git
 bitclust setup
 
 # Xcode8以降にalcatrazを導入する場合、以下のコマンド実行が必要になる
-#   update_xcode_plugins --unsign
 # Xcodeのバージョンとxcodebuildコマンドも対象にするか聞かれるので対象にして実行する
+update_xcode_plugins
+update_xcode_plugins --unsign
 
 # node.js
 npm install -g \
