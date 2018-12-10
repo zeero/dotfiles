eeddec9ed8cc1f41fe4485da6d02507fc73cdcaf
zeero <zeero26@gmail.com>
2018-12-10 14:44:24 +0900

    plist設定をスクリプト化

--- install.sh
+++ install.sh
@@ -124,6 +124,30 @@ pip3 install \
   vim-vint \
   solargraph-utils.py \
 
+# plist
+## OS
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+defaults write "com.apple.AppleMultitouchTrackpad" "TrackpadCornerSecondaryClick" -int 2
+defaults write "com.apple.AppleMultitouchTrackpad" "TrackpadRightClick" -bool false
+defaults write "com.apple.driver.AppleBluetoothMultitouch.trackpad" "TrackpadCornerSecondaryClick" -int 2
+defaults write "com.apple.driver.AppleBluetoothMultitouch.trackpad" "TrackpadRightClick" -bool false
+defaults write "com.apple.dock" "showAppExposeGestureEnabled" -bool true
+defaults write "com.apple.preference.general" "version" -int 1
+defaults write "com.apple.AppleMultitouchTrackpad" "DragLock" -bool True
+defaults write "com.apple.AppleMultitouchTrackpad" "Clicking" -bool True
+defaults write "com.apple.AppleMultitouchTrackpad" "Dragging" -bool True
+defaults write "com.apple.driver.AppleBluetoothMultitouch.trackpad" "DragLock" -bool True
+defaults write "com.apple.driver.AppleBluetoothMultitouch.trackpad" "Clicking" -bool True
+defaults write "com.apple.driver.AppleBluetoothMultitouch.trackpad" "Dragging" -bool True
+## Dock
+defaults write "com.apple.dock" "tilesize" -int 32
+defaults write "com.apple.dock" "magnification" -bool true
+defaults write "com.apple.dock" "autohide" -bool true
+## iTerm2
+defaults write "com.googlecode.iterm2" "QuitWhenAllWindowsClosed" -bool true
+## Clipy
+defaults write "com.clipy-app.Clipy" "kCPYPrefNumberOfItemsPlaceInlineKey" -int 10
+
 # その他設定
 echo "・キー移動を速くするために「システム環境設定」の「キーボード」からキーリピート、リピート認識を最大にする"
 echo "・ダブルタップでドラッグするために「システム環境設定」の「アクセシビリティ」→「マウスとトラックパッド」→「トラックパッドオプション」→「ドラッグロック」を有効にする"
