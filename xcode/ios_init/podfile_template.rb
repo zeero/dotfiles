1ca71e94da28f45f52730b9b653fd7ee14962de0
zeero <zeero26@gmail.com>
2019-05-20 17:33:56 +0900

    微調整

--- xcode/ios_init/podfile_template.rb
+++ xcode/ios_init/podfile_template.rb
@@ -47,7 +47,7 @@ post_install do |installer|
   installer.pods_project.targets.each do |target|
     target.build_configurations.each do |config|
       <%- if options[:swift4_2] -%>
-      config.build_settings['SWIFT_VERSION'] = "4.2"
+      config.build_settings['SWIFT_VERSION'] = '4.2'
       config.build_settings['ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES'] = 'YES'
       <%- end -%>
     end
