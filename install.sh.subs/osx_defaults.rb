25891093547c9ab37603c4cdddbbc9d2cf6a02aa
zeero <zeero26@gmail.com>
2020-03-26 17:50:38 +0900

    osx_defaults.rb作成

---
+++ install.sh.subs/osx_defaults.rb
@@ -0,0 +1,22 @@
+require 'yaml'
+
+yaml = YAML.load_file(File.expand_path('osx_defaults.yml', __dir__))
+
+yaml.each do | defaults_key, items |
+  items.each do | key, value |
+    case value
+    when TrueClass, FalseClass
+      type = 'bool'
+    when Integer
+      type = 'int'
+    when Float
+      type = 'float'
+    when String
+      type = 'string'
+    else
+      next
+    end
+    system "defaults write #{defaults_key} #{key} -#{type} #{value}"
+  end
+end
+
