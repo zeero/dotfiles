e273036bb946ee5410e60ecc5cf3fccdf1fbbdc6
zeero <zeero26@gmail.com>
2023-04-24 11:00:42 +0900

    [symlink] .config配下をリンク設定

--- install.sh.subs/symlink.sh
+++ install.sh.subs/symlink.sh
@@ -10,6 +10,7 @@ ln -sf ${DOTFILES}/home/.tmux/plugins/tpm ~/.tmux/plugins/tpm
 ln -sf ${DOTFILES}/vim ~/.vim
 ln -sf ${DOTFILES}/vim/vimrc ~/.vimrc
 mkdir -p ~/.config
+find ${DOTFILES}/home/.config -type d -depth 1 -exec ln -sf {} ~/.config \;
 ln -sf ${DOTFILES}/vim ~/.config/nvim
 ln -sf ${DOTFILES}/vim/vimrc ~/.config/nvim/init.vim
 mkdir -p ${DOTFILES}/vim/after/autoload
